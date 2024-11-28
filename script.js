7d024cda36a99ac236dbcc37dbdc1965ce462e24
KristeneCavan <kristenecahan888@gmail.com>
2024-11-29 00:49:52 +0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const sliderTabs = document.querySelectorAll(".slider-tab");
+const sliderIndicator = document.querySelectorAll(".slider-indicator");
 
-const swiper = new Swiper('.slider-container', {
+const updatePagination = (tab, index) => {
+    sliderIndicator.style.transform = 'translateX(${tab.offsetLeft - 20}px)';
+}
+const swiper = new Swiper(".slider-container", {
     effect: "slide",
     speed: 1300,
 });
@@ -8,5 +12,6 @@ const swiper = new Swiper('.slider-container', {
 sliderTabs.forEach((tab, index) => {
     tab.addEventListener("click", () => {
         swiper.slideTo(index);
+        updatePagination(tab, index);
     });
 });
