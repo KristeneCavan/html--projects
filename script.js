3108e5a3acd77c6ca7be381dcf5ac90ee5a87ab4
KristeneCavan <kristenecahan888@gmail.com>
2024-11-29 06:27:10 +0800

    update

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-const sliderTabs = document.querySelectorAll(".slider-tab");
-const sliderIndicator = document.querySelectorAll(".slider-indicator");
+const navbarLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
 
-const updatePagination = (tab, index) => {
-    sliderIndicator.style.transform = 'translateX(${tab.offsetLeft - 20}px)';
-}
-const swiper = new Swiper(".slider-container", {
-    effect: "slide",
-    speed: 1300,
+menuOpenButton.addEventListener("click", () => {
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
 });
 
-sliderTabs.forEach((tab, index) => {
-    tab.addEventListener("click", () => {
-        swiper.slideTo(index);
-        updatePagination(tab, index);
-    });
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Close menu when nav link is clicked
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
+
+/* Initializing Swiper */
+let swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  /* Responsive breakpoints */
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
